57a5289105d68c44f5c09dadb77b0037dd5cfb28
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2022-09-06 21:37:07 +0200

    Update setup.py and year in license

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-#-*- coding: utf-8 -*-
-from distutils.core import setup
+import setuptools
 
-setup(
-  name = 'vphasfits',
-  version = '0.1',
-  description = 'A library allows to get data from the multi-extension VPHAS+ fits',
-  install_requires=['pyfits',
-                    'numpy',
-                    'astropy'
-                    ],
-  author = 'Przemysław Bruś',
-  license='MIT',
-  url = 'https://github.com/pbrus/vphasfits',
-  download_url = 'https://github.com/pbrus/vphasfits/archive/0.1.tar.gz',
-  packages = ['vphasfits'],
-  keywords = ['VPHAS+', 'fits', 'text'],
-  classifiers = [],
+setuptools.setup(
+    name="vphasfits",
+    version="1.0.0",
+    author="Przemysław Bruś",
+    description="A library allows to get data from the multi-extension VPHAS+ fits",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "astropy",
+        "numpy",
+    ],
+    scripts=[
+        "scripts/vphas_srctbl.py",
+        "scripts/vphas_pawprint.py",
+        "scripts/vphas_cat.py",
+    ],
+    python_requires=">=3.6",
 )
